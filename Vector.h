82a1aae1682220ef0e931d3fef38f3e642ad4318
Christian Pena <35936591+cpena18@users.noreply.github.com>
2018-02-28 19:20:00 -0500

    Update Vector.h
    Teacher, @rivlev I fixed compile errors, please let me know if there is any issues. Thank you

--- Vector.h
+++ Vector.h
@@ -1,217 +1,142 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 #include <iostream>
-
-
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <stdexcept>    //I changed exception for stdexcept
 
 using namespace std;
-
-
 
 template<class T>
 
 class Vector{
-    
-    
-    
+
+
+
 private:
-    
     T* arr;
-    
-    int size;
-    
-    int capacity;
-    
-    
-    
-    void grow(){
-        
+    int sz;             //I changed the data member name from size to sz since having size
+    int capacity;       //as a data member and function create a conflict, I chose to change
+                        //size here because teacher used size to call functions in main
+    void grow( ){
         capacity = capacity*2;
-        
-        T* bigArr = new T[capacity]; // creates a new array, twice the size of original
-        
-        
-        
-        for(int i = 0; i < size; i++){
-            
+        T* bigArr = new T[capacity]; // creates a new array, twice the sz of original
+
+        for(int i = 0; i < sz; i++){
             bigArr[i] = arr[i];
-            
         }
-        
-        
-        
+
         delete [] arr; // does NOT delete 'arr'. deletes the array 'arr' is pointing
-        
         // arr is now pointing at nothing (dangling pointer)
-        
-        
-        
         arr = bigArr; // point 'arr' to where 'bigArr'
-        
     }
-    
-    
-    
+
+
+
 public:
-    
+
     Vector(){ // default ctor
-        
-        size = 0;
-        
+        sz = 0;
         capacity = 8;
-        
         arr = new T[capacity]; // returns a pointer to [0] (arr now points ot [0] elemens)
-        
     }
-    
-    
-    
+
+
+
     Vector(int s){
-        
-        size = s;
-        
-        capacity = size*2;
-        
+        sz = s;
+        capacity = sz*2;
         arr = new T[capacity];
-        
     }
-    
-    
-    
+
     Vector(int s, T val){ // fill ctor
-        
-        size = s;
-        
-        capacity = size*2;
-        
+        sz = s;
+        capacity = sz*2;
         arr = new T[capacity];
-        
-        
-        
-        for(int i = 0; i < size; i++){
-            
+
+        for(int i = 0; i < sz; i++){
             arr[i] = val; // *(arr+i) = val
-            
         }
-        
     }
-    
-    
+
     void push_back(T val){
-        
-        if(size == capacity){
-            
+        if(sz == capacity){
             grow();
-            
         }
-        
-        
-        
-        arr[size] = val;
-        
-        size++;
-        
+        arr[sz] = val;
+        sz++;
     }
-    
-    
-    
+
     void pop_back(){
-        
-        if(size > 0){
-            
-            size--;
-            
+        if(sz > 0){
+            sz--;
         }
-        
     }
-    
-    
-    
+
     void insert(const T& val,int pos){
-        
-        if(pos < 0 || pos >= size){
-            
+        if(pos < 0 || pos >= sz){
             throw std::overflow_error("Error: index out-of-bounds");
-            
             return;
-            
         }
-        
-        if(size == capacity){
-            
+
+        if(sz == capacity){
             grow();
-            
         }
-        
-        
-        
-        for(int i = size; i > pos; i--){
-            
+
+        for(int i = sz; i > pos; i--){
             arr[i] = arr[i-1];
-            
         }
-        
-        
-        
+
         arr[pos] = val;
-        
-        size++;
-        
+        sz++;
     }
-    
-    
-    
+
     void erase(int pos){
-        
-        if(pos < 0 || pos >= size){
-            
+        if(pos < 0 || pos >= sz){
            // cout << "Error: index out-of-bounds" << endl;
             throw std::overflow_error("Error: index out-of-bounds");
             return;
-            
         }
-        
-        
-        
-        for(int i = pos; i < size - 1; i++){
-            
+
+        for(int i = pos; i < sz - 1; i++){
             arr[i] =  arr[i+1];
-            
         }
-        
-        size--;
-        
+
+        sz--;
     }
-    
-    
-    
-    int Size(){     // this is a getter
-        
-        return size;
-        
+
+    int size(){     // this is a getter
+        return sz;
     }
-    
+
     bool empty() {
-        return size == 0;
+        return sz == 0;
     }
+
     T& front() {
         return arr[0];
     }
-    
+
     T& at(int pos) {
-        if(pos >= size || pos < 0){
+        if(pos >= sz || pos < 0){
             throw std::overflow_error("not legal position");
         }
         return arr[pos];
     }
+
     T& operator[](int pos) {
+        if(pos >= sz || pos < 0){
+            throw std::overflow_error("not legal position");
+        }
         return arr[pos];
     }
+
     T& back() {
-        return arr[size-1];
+        return arr[sz-1];
     }
-    
 };
+
 
 
 #endif
